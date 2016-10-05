4517bdff661650d5246744cff32dceaddfcccd5d
jensdebernitz <jensdebernitz@googlemail.com>
2016-10-05 14:11:19 +0200

    more

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxErgebnis = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxStadt = new System.Windows.Forms.TextBox();
+            this.textBoxLand = new System.Windows.Forms.TextBox();
+            this.textBoxStadion = new System.Windows.Forms.TextBox();
+            this.textBoxGastmanschft = new System.Windows.Forms.TextBox();
+            this.textBoxHeimmanschaft = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(483, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Ergebnis";
+            // 
+            // textBoxErgebnis
+            // 
+            this.textBoxErgebnis.Location = new System.Drawing.Point(486, 75);
+            this.textBoxErgebnis.Name = "textBoxErgebnis";
+            this.textBoxErgebnis.Size = new System.Drawing.Size(100, 20);
+            this.textBoxErgebnis.TabIndex = 31;
+            this.textBoxErgebnis.Text = "1:1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Speichern";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(314, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Land";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(163, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Stadt";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Stadion";
+            // 
+            // dateTimePickerDate
+            // 
+            this.dateTimePickerDate.CustomFormat = "MM/dd/yy";
+            this.dateTimePickerDate.Location = new System.Drawing.Point(314, 28);
+            this.dateTimePickerDate.Name = "dateTimePickerDate";
+            this.dateTimePickerDate.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerDate.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(311, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Datum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Gastmanschaft";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Heimmanschaft";
+            // 
+            // textBoxStadt
+            // 
+            this.textBoxStadt.Location = new System.Drawing.Point(166, 75);
+            this.textBoxStadt.Name = "textBoxStadt";
+            this.textBoxStadt.Size = new System.Drawing.Size(145, 20);
+            this.textBoxStadt.TabIndex = 22;
+            this.textBoxStadt.Text = "Koblenz";
+            // 
+            // textBoxLand
+            // 
+            this.textBoxLand.Location = new System.Drawing.Point(317, 75);
+            this.textBoxLand.Name = "textBoxLand";
+            this.textBoxLand.Size = new System.Drawing.Size(145, 20);
+            this.textBoxLand.TabIndex = 21;
+            this.textBoxLand.Text = "Deutschland";
+            // 
+            // textBoxStadion
+            // 
+            this.textBoxStadion.Location = new System.Drawing.Point(15, 75);
+            this.textBoxStadion.Name = "textBoxStadion";
+            this.textBoxStadion.Size = new System.Drawing.Size(145, 20);
+            this.textBoxStadion.TabIndex = 20;
+            this.textBoxStadion.Text = "Koblenz";
+            // 
+            // textBoxGastmanschft
+            // 
+            this.textBoxGastmanschft.Location = new System.Drawing.Point(163, 28);
+            this.textBoxGastmanschft.Name = "textBoxGastmanschft";
+            this.textBoxGastmanschft.Size = new System.Drawing.Size(145, 20);
+            this.textBoxGastmanschft.TabIndex = 19;
+            this.textBoxGastmanschft.Text = "Eintracht Frankfurt";
+            // 
+            // textBoxHeimmanschaft
+            // 
+            this.textBoxHeimmanschaft.Location = new System.Drawing.Point(12, 28);
+            this.textBoxHeimmanschaft.Name = "textBoxHeimmanschaft";
+            this.textBoxHeimmanschaft.Size = new System.Drawing.Size(145, 20);
+            this.textBoxHeimmanschaft.TabIndex = 18;
+            this.textBoxHeimmanschaft.Text = "Tus Koblenz";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 153);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1036, 193);
+            this.dataGridView1.TabIndex = 35;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(843, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Auslesen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(940, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "Beenden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 459);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxErgebnis);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePickerDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxStadt);
+            this.Controls.Add(this.textBoxLand);
+            this.Controls.Add(this.textBoxStadion);
+            this.Controls.Add(this.textBoxGastmanschft);
+            this.Controls.Add(this.textBoxHeimmanschaft);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxErgebnis;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxStadt;
+        private System.Windows.Forms.TextBox textBoxLand;
+        private System.Windows.Forms.TextBox textBoxStadion;
+        private System.Windows.Forms.TextBox textBoxGastmanschft;
+        private System.Windows.Forms.TextBox textBoxHeimmanschaft;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,202 @@ namespace GroundHopping
 {
     public partial class Form1 : Form
     {
+        private OleDbConnection con;
+        private OleDbCommand sql;
+
         public Form1()
         {
             InitializeComponent();
+
+            string connect = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                 "Data Source=..\\groundHooping.accdb";
+            //string connect = "Provider=Microsoft.JET.OLEDB.4.0;" +
+            //     "Data Source=..\\groundHooping.mdb";
+
+            con = new OleDbConnection(connect);
+            con.Open();
+
+            sql = con.CreateCommand();
+
+            // Set the Format type and the CustomFormat string.
+            dateTimePickerDate.Format = DateTimePickerFormat.Custom;
+            dateTimePickerDate.CustomFormat = "dd.MM.yyyy";
+
+            sql.CommandText = "select * from groundHooping;";
+            OleDbDataReader reader = sql.ExecuteReader();
+
+            string[] ManschaftsArray;
+            string[] stadionArray;
+            string[] landArray;
+            string[] stadtArray;
+
+            ManschaftsArray = new string[0];
+            stadionArray = new string[0];
+            landArray = new string[0];
+            stadtArray = new string[0];
+
+
+            while (reader.Read())
+            {
+                string neuerVerein = reader[1].ToString();
+
+                if (isNotInList(ManschaftsArray, neuerVerein))
+                {
+                    Array.Resize(ref ManschaftsArray, ManschaftsArray.Length + 1);
+                    ManschaftsArray[ManschaftsArray.Length - 1] = neuerVerein;
+                }
+
+                neuerVerein = reader[2].ToString();
+
+                if (isNotInList(ManschaftsArray, neuerVerein))
+                {
+                    Array.Resize(ref ManschaftsArray, ManschaftsArray.Length + 1);
+                    ManschaftsArray[ManschaftsArray.Length - 1] = neuerVerein;
+                }
+
+
+                neuerVerein = reader[4].ToString();
+
+                if (isNotInList(stadionArray, neuerVerein))
+                {
+                    Array.Resize(ref stadionArray, stadionArray.Length + 1);
+                    stadionArray[stadionArray.Length - 1] = neuerVerein;
+                }
+
+                neuerVerein = reader[5].ToString();
+
+                if (isNotInList(stadtArray, neuerVerein))
+                {
+                    Array.Resize(ref stadtArray, stadtArray.Length + 1);
+                    stadtArray[stadtArray.Length - 1] = neuerVerein;
+                }
+
+                neuerVerein = reader[6].ToString();
+
+                if (isNotInList(landArray, neuerVerein))
+                {
+                    Array.Resize(ref landArray, landArray.Length + 1);
+                    landArray[landArray.Length - 1] = neuerVerein;
+                }
+
+
+            }
+            reader.Close();
+
+            // Create the list to use as the custom source. 
+            var sourceManschaften = new AutoCompleteStringCollection();
+            sourceManschaften.AddRange(ManschaftsArray);
+
+            // Create the list to use as the custom source. 
+            var sourceStadion = new AutoCompleteStringCollection();
+            sourceStadion.AddRange(stadionArray);
+
+            // Create the list to use as the custom source. 
+            var sourceStadt = new AutoCompleteStringCollection();
+            sourceStadt.AddRange(stadtArray);
+
+            // Create the list to use as the custom source. 
+            var sourceLand = new AutoCompleteStringCollection();
+            sourceLand.AddRange(landArray);
+
+            textBoxHeimmanschaft.AutoCompleteCustomSource = sourceManschaften;
+            textBoxHeimmanschaft.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBoxHeimmanschaft.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            textBoxGastmanschft.AutoCompleteCustomSource = sourceManschaften;
+            textBoxGastmanschft.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBoxGastmanschft.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            textBoxStadion.AutoCompleteCustomSource = sourceStadion;
+            textBoxStadion.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBoxStadion.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            textBoxStadt.AutoCompleteCustomSource = sourceStadt;
+            textBoxStadt.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBoxStadt.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            textBoxLand.AutoCompleteCustomSource = sourceLand;
+            textBoxLand.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBoxLand.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+
+        bool isNotInList(string[] inList, string newValue)
+        {
+            bool returnValue = true;
+
+            for (int i = 0; i < inList.Length; i++)
+            {
+                if (newValue == inList[i])
+                {
+                    returnValue = false;
+                }
+            }
+            return returnValue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sql.CommandText = "select * from groundHooping;";
+            OleDbDataReader reader = sql.ExecuteReader();
+            dataGridView1.Columns.Clear();
+
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                dataGridView1.Columns.Add(reader.GetName(i), reader.GetName(i));
+            }
+
+            while (reader.Read())
+            {
+                object[] row = new object[reader.FieldCount];
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row[i] = reader[i];
+                }
+
+                dataGridView1.Rows.Add(row);
+            }
+            reader.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sql.CommandText = "insert into groundHooping(Heimmanschaft, Gastmanschaft, Datum, Stadion, Stadt, Land, Ergebnis)" +
+                   " values('" + textBoxHeimmanschaft.Text + "', '"
+                              + textBoxGastmanschft.Text + "', '"
+                              + dateTimePickerDate.Text + "', '"
+                              + textBoxStadion.Text + "', '"
+                              + textBoxStadt.Text + "', '"
+                              + textBoxLand.Text + "', '"
+                              + textBoxErgebnis.Text + "');";
+            sql.ExecuteNonQuery();
+
+            if (textBoxHeimmanschaft.Text.ToLower() == "1.fc köln"
+                || textBoxGastmanschft.Text.ToLower() == "1.fc köln")
+            {
+                // Initializes the variables to pass to the MessageBox.Show method.
+
+                string message = "Erster Fussball Club Köln,\r\nErster Fussball Club Köln,\r\nErster Fussball Club Köln,\r\nErster FC Köln... ";
+                string caption = "Erster Fussballclub Köln";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                // Displays the MessageBox.
+
+                result = MessageBox.Show(message, caption, buttons);
+
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+
+                }
+            }
         }
     }
 }
