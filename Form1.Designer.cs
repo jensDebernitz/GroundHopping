7901e519a958b8f72f709512ac5703edfd474b0b
jensdebernitz <jensdebernitz@googlemail.com>
2016-10-05 14:14:47 +0200

    change window Name

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -231,7 +231,7 @@
             this.Controls.Add(this.textBoxGastmanschft);
             this.Controls.Add(this.textBoxHeimmanschaft);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ground Hopping";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
